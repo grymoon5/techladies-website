de678bee02c8819a8d430514cca50deddf68bb9b
Aishwarya <eaishwarya273@gmail.com>
2025-07-29 18:20:55 +0800

    Update mentorship-2025-mentors.js
    Added Sabrina L's profile

--- src/data/mentorship-2025-mentors.js
+++ src/data/mentorship-2025-mentors.js
@@ -86,6 +86,19 @@ export const mentorsData = {
         ),
       },
       {
+        title: "Sabrina L",
+        link: "https://www.linkedin.com/in/sabrinal-498970113/",
+        description: (
+          <>
+          backend engineer looking for interesting problems to solve, 
+          specifically in the gaming, fintech and identity industry. sales engineering sounds fun, too - sab loves yapping.
+          I think of myself as a builder rather than just "software engineer", 
+          and I have been involved in varying degrees in devops, architecture, engineering, and data governance. 
+          I love working as a part of a team, but am also great at "solo missions" (though it does get lonely 🐈 ).
+          </>
+        ),
+      },
+      { 
         title: "Bala Sivagnanam",
         link: "https://www.linkedin.com/in/balasivgn/",
         description: (
